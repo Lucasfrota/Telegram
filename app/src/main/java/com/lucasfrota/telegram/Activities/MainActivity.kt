1cb9eb2a17d273f5532502107fcd26d147935612
Lucas Frota <lucv.frota@gmail.com>
2019-02-05 15:05:15 -0400

    5 - creating a folder structure

--- app/src/main/java/com/lucasfrota/telegram/Activities/MainActivity.kt
+++ app/src/main/java/com/lucasfrota/telegram/Activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lucasfrota.telegram
+package com.lucasfrota.telegram.Activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -8,6 +8,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.lucasfrota.telegram.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -24,7 +25,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
