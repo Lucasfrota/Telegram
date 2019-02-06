5ecd699b907b3a166fdc45e3a43fc5bb57770683
Lucas Frota <lucv.frota@gmail.com>
2019-02-06 09:27:10 -0400

    9 - Editing ChatAdapter file

--- app/src/main/java/com/lucasfrota/telegram/Adapters/ChatAdapter.kt
+++ app/src/main/java/com/lucasfrota/telegram/Adapters/ChatAdapter.kt
@@ -1,4 +1,20 @@
 package com.lucasfrota.telegram.Adapters
 
-class ChatAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lucasfrota.telegram.Holders.ChatHolder
+import com.lucasfrota.telegram.Models.ChatItem
+
+class ChatAdapter(private val itens: List<ChatItem>): RecyclerView.Adapter<ChatHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ChatHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
