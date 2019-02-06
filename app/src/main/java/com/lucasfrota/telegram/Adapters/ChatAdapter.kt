5a8b4b6fa0651b6c88a764c249f5d331540a7678
Lucas Frota <lucv.frota@gmail.com>
2019-02-06 10:02:25 -0400

    11 - Editing onCreateView and getItemCount methods

--- app/src/main/java/com/lucasfrota/telegram/Adapters/ChatAdapter.kt
+++ app/src/main/java/com/lucasfrota/telegram/Adapters/ChatAdapter.kt
@@ -1,20 +1,23 @@
 package com.lucasfrota.telegram.Adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lucasfrota.telegram.Holders.ChatHolder
 import com.lucasfrota.telegram.Models.ChatItem
+import com.lucasfrota.telegram.R
 
 class ChatAdapter(private val itens: List<ChatItem>): RecyclerView.Adapter<ChatHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_item, parent, false)
+        return ChatHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return itens.size
     }
 
     override fun onBindViewHolder(holder: ChatHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
